d3175528357613dcfe2aa719101967d04e238cac
Adeel <adeelz92@localhost.localdomain>
2023-06-09 12:52:04 -0500

    added vtk

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "adder.h"
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+
+int main(int argc, char* argv[]) {
+	std::cout << adeelmath::add(1, 2) << std::endl;
+	return 0;
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+configure and build with cmake using cmake-gui or use command line
+
+linux command line ez mode
+
+    ./configure.h
+    ./build.h
+    ./run.sh
